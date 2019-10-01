8d1d3eeae43677088af607404ed76913907f9b89
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-01 15:25:19 -0400

    adds edit for profile name

--- ImagePicker-Lab/Controllers/ProfileViewController.swift
+++ ImagePicker-Lab/Controllers/ProfileViewController.swift
@@ -10,7 +10,17 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    var mode: Mode = .notEditing
+    
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileLabel: UILabel!
+    @IBOutlet weak var profileNameTextField: UITextField!
+    @IBOutlet weak var editButton: UIButton!
+    
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        hideOrShowLabels()
+    }
     
     @IBAction func buttonPressed(_ sender: UIBarButtonItem) {
         openImagePicker()
@@ -23,25 +33,73 @@ class ProfileViewController: UIViewController {
         }
     }
     private func setDefaults() {
-        if let profilePic = UserDefaultWrapper.manager.getPic() {
+        if let profilePic = UserDefaultWrapper.manager.getPic(), let profileName = UserDefaultWrapper.manager.getName() {
             profileImageView.image = profilePic
+            profileLabel.text =  profileName
         } else {
             return
         }
+        
+    }
+    private func hideOrShowLabels() {
+        switch mode {
+        case .editing:
+            if let text = profileNameTextField.text {
+                profileLabel.text = text
+                profileLabel.textColor = .white
+                UserDefaultWrapper.manager.set(name: text)
+            }
+            
+            profileNameTextField.isHidden = true
+            profileLabel.isHidden = false
+            mode = .notEditing
+            editButton.setTitle("Edit", for: .normal)
+        case .notEditing:
+            profileNameTextField.isHidden = false
+            profileLabel.isHidden = true
+            mode = .editing
+            editButton.setTitle("Done", for: .normal)
+        }
+    }
+    private func makeGradientBackground() {
+        let layer = CAGradientLayer()
+        layer.frame = view.bounds
+        layer.colors = [UIColor.cyan.cgColor,UIColor.purple.cgColor]
+        layer.startPoint = CGPoint(x: 0, y: 0)
+        layer.endPoint = CGPoint(x: 1, y: 1)
+        self.view.layer.insertSublayer(layer, at: 0)
+    }
+    private func makeBorderForName() {
+        profileLabel.layer.borderColor = UIColor.white.cgColor
+        profileLabel.layer.borderWidth = 2
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setDefaults()
+        profileNameTextField.isHidden = true
+        profileNameTextField.delegate = self
+        makeGradientBackground()
+        makeBorderForName()
     }
     
 }
 
+//MARK: - Extensions
 extension ProfileViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.originalImage] as? UIImage else {return}
         profileImageView.image = image
         UserDefaultWrapper.manager.setPic(image: image)
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ProfileViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text {
+            profileLabel.text = text
+        }
+        return true
     }
 }
--- ImagePicker-Lab/Model/UserDefaults.swift
+++ ImagePicker-Lab/Model/UserDefaults.swift
@@ -16,15 +16,27 @@ class UserDefaultWrapper {
     static let manager = UserDefaultWrapper()
     
     private let profilePicKey = "profilePic"
+    private let profileNameKey = "profileName"
     
     func setPic(image: UIImage) {
         if let imageData = image.pngData() {
         UserDefaults.standard.set(imageData, forKey: profilePicKey)
         }
     }
+    func set(name:String) {
+        UserDefaults.standard.set(name, forKey: profileNameKey)
+    }
+    
+    
     func getPic() -> UIImage? {
         if let picData = UserDefaults.standard.value(forKey: profilePicKey) as? Data, let image = UIImage(data: picData) {
             return image
+        }
+        return nil
+    }
+    func getName() -> String? {
+        if let name = UserDefaults.standard.value(forKey: profileNameKey) as? String {
+            return name
         }
         return nil
     }
